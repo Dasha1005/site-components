23df7ba3ff3c756791cbbd0a300db2c745fa669d
Dasha <rosinecdasa@gmail.com>
2021-06-01 13:36:08 +0300

    delete html1

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ $(function () {
         $(dataId).addClass('active-tab');
     });
 });
+//progress-bar
+$(function(){
+    var owl  = $('.object-types .owl-carousel');
+    owl.owlCarousel();
+    owl.on('changed.owl.carousel', function(event) {
+        var page = event.page.index + 1;
+        var item = (100 / event.item.count);
+        var progressBar = $('.progress span');
+        var result = parseInt((page)  * (item));
+        $(progressBar).css('max-width', result + '%');
+    });
+    owl.on('resized.owl.carousel', function(event){
+        owl.trigger('to.owl.carousel', [0, 400]);
+    });
+});
